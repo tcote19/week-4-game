5d67451cd6082febe44d767c847a8d0f3eac8ca6
thomas cote <tcote19@gmail.com>
2017-11-14 16:24:18 -0800

    Updated js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,38 +1,75 @@
 
 $( document ).ready(function() {
     var randomNum = Math.floor((Math.random() * 120) + 19);
+    $("#random-number").append(randomNum);
 
     var crystal1 = $("#crystal-one");
     var crystal2 = $("#crystal-two");
     var crystal3 = $("#crystal-three");
     var crystal4 = $("#crystal-four");
 
-    $("#random-number").append(randomNum);
+    var btnVal1 = Math.floor((Math.random() * 12) + 1);
+    var btnVal2 = Math.floor((Math.random() * 12) + 1);
+    var btnVal3 = Math.floor((Math.random() * 12) + 1);
+    var btnVal4 = Math.floor((Math.random() * 12) + 1);
+
+    var playerScore = $("#player-score");
+    var scorePlacehold = 0;
+
+
+
     
+    
+
+
     $(crystal1).hover(function() {
-    	$("#crystal-denomination1").append(Math.floor((Math.random() * 19) + 1));
+    	$("#crystal-denomination1").append(btnVal1);
     	$("#crystal-denomination1").animate({'opacity':0}, 'slow');
     	$(this).unbind('mouseenter mouseleave');
 	});
 
     $(crystal2).hover(function() {
-    	$("#crystal-denomination2").append(Math.floor((Math.random() * 19) + 1));
+    	$("#crystal-denomination2").append(btnVal2);
     	$("#crystal-denomination2").animate({'opacity':0}, 'slow');
     	$(this).unbind('mouseenter mouseleave');
 	});
 
     $(crystal3).hover(function() {
-    	$("#crystal-denomination3").append(Math.floor((Math.random() * 19) + 1));
+    	$("#crystal-denomination3").append(btnVal3);
     	$("#crystal-denomination3").animate({'opacity':0}, 'slow');
     	$(this).unbind('mouseenter mouseleave');
 	});
 
     $(crystal4).hover(function() {
-    	$("#crystal-denomination4").append(Math.floor((Math.random() * 19) + 1));
+    	$("#crystal-denomination4").append(btnVal4);
     	$("#crystal-denomination4").animate({'opacity':0}, 'slow');
     	$(this).unbind('mouseenter mouseleave');
 	});
 
+    $(crystal1).on("click", function() {
+		if (btnVal1 <= 12) {
+			$(scorePlacehold += btnVal1);
+			$(playerScore).html(scorePlacehold);
+		}
+	});
+    $(crystal2).on("click", function() {
+		if (btnVal2 <= 12) {
+			$(scorePlacehold += btnVal2);
+			$(playerScore).html(scorePlacehold);
+		}
+	});
+    $(crystal3).on("click", function() {
+		if (btnVal3 <= 12) {
+			$(scorePlacehold += btnVal3);
+			$(playerScore).html(scorePlacehold);
+		}
+	});
+    $(crystal4).on("click", function() {
+		if (btnVal4 <= 12) {
+			$(scorePlacehold += btnVal4);
+			$(playerScore).html(scorePlacehold);
+		}
+	});    
 });
 
 
